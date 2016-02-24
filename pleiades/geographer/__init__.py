fe7c55dd48da80caeffef10c91e6a42a4494605a
David Glick <david@glicksoftware.com>
2016-02-24 11:32:44 -0800

    patch collective.geo.geographer to make sure it looks in the right place

--- pleiades/geographer/__init__.py
+++ pleiades/geographer/__init__.py
@@ -1,5 +1,4 @@
-# package
-#
-
-from pleiades.geographer import catalog
-
+# Make sure collective.geo.geographer continues to read/write
+# geographic annotations in the same location that zgeo.geographer did
+import collective.geo.geographer.geo
+collective.geo.geographer.geo.KEY = 'zgeo.geographer.georeference'
