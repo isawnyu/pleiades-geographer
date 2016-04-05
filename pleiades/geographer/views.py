b7c6dbc11156f63a82d9e1fcf2921ece52e209f6
David Glick <david@glicksoftware.com>
2016-04-05 08:50:04 -0700

    pep8

--- pleiades/geographer/views.py
+++ pleiades/geographer/views.py
@@ -1,15 +1,13 @@
-
+from pleiades.geographer.geo import explode
+from pleiades.geographer.interfaces import IExtent
+from pleiades.geographer.interfaces import IRepresentativePoint
 from zope.publisher.browser import BrowserView
 
-from pleiades.geographer.interfaces import IRepresentativePoint
-from pleiades.geographer.interfaces import IExtent
-from pleiades.geographer.geo import explode
 
 class Where(BrowserView):
     def __call__(self):
         pt = IRepresentativePoint(self.context).coords
         extent = IExtent(self.context).extent
-        bounds = None
         if extent and pt:
             coords = list(explode(extent['coordinates']))
             xy = zip(*coords)
@@ -17,10 +15,10 @@ class Where(BrowserView):
             maxx = max(xy[0])
             miny = min(xy[1])
             maxy = max(xy[1])
-            return { 
-                'bbox': (minx, miny, maxx, maxy), 
-                'reprPt': pt, 
-                'extent': extent }
+            return {
+                'bbox': (minx, miny, maxx, maxy),
+                'reprPt': pt,
+                'extent': extent,
+            }
         else:
             return None
-
