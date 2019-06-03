6e5f033e7a119c9cdf992007d661bb9e7bd07be2
Alec Mitchell <alecpm@gmail.com>
2019-06-03 15:46:35 -0700

    Remove ATVM dependency.

--- pleiades/geographer/tests/base.py
+++ pleiades/geographer/tests/base.py
@@ -4,7 +4,6 @@ from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
 from Testing import ZopeTestCase as ztc
 
-ztc.installProduct('ATVocabularyManager')
 ztc.installProduct('Products.CompoundField')
 ztc.installProduct('Products.ATBackRef')
 ztc.installProduct('Products.OrderableReferenceField')
